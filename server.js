76762358090562a0219f8e83d4a146264b51f537
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-09-05 17:38:14 -0700

    adding db functions

--- server.js
+++ server.js
@@ -1,42 +1,126 @@
 const db = require('./db/connection');
-const mysql  = require('mysql2')
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
-// Connect to database
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // MySQL password
-      password: '',
-      database: 'inventory_db'
-    },
-    console.log(`Connected to the inventory_db database.`)
-  );
 
-// Query database
-//View All Departments
-db.query(`SELECT * FROM department;`, function (err, results) {
-    console.log(results);
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+});
+
+const promptMain = () => {
+  inquirer.prompt({
+    type: 'list',
+    name: 'main',
+    message: 'What would you like to do?',
+    choices: [
+      'View All Departments',
+      'View All Roles',
+      'View All Employees',
+      'Add a Department',
+      'Add a Role',
+      'Add an Employee',
+      'Remove an Employee',
+      'Update an Employee Role']
+  })
+    .then((answers) => {
+      if (answers.main === 'View All Departments') {
+        viewDept();
+      } else if (answers.main === 'View All Roles') {
+        viewRole();
+      } else if (answers.main === 'View All Employees') {
+        viewEmployee();
+      } else if (answers.main === 'Add a Department') {
+        addDept();
+      } else if (answers.main === 'Add a Role') {
+        addRole();
+      } else if (answers.main === 'Add an Employee') {
+        addEmployee();
+      } else if (answers.main === 'Remove an Employee') {
+        removeEmployee();
+      } else if (answers.main === 'Update an Employee Role') {
+        updateEmployee();
+      }
+    })
+};
+
+// // Query database
+// //View All Departments
+
+const viewDept = () => {
+  const sql = `SELECT * FROM department`;
+  db.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    console.table(results);
+    start();
   });
+};
 
-//View All Roles
-db.query(`SELECT 
-role.id, role.title,department.name as department, role.salary
-FROM role
-JOIN department ON role.department_id=department.id;`, function (err, results) {
-    console.log(results);
+// //View All Roles
+
+const viewRole = () => {
+  const sql = `SELECT 
+  role.id, role.title,department.name as department, role.salary
+  FROM role
+  JOIN department ON role.department_id=department.id`;
+  db.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    console.table(results);
+    start();
   });
+}
 
-//View All Employees
-db.query(`SELECT
--> e.id, e.first_name, e.last_name, role.title, department.name, role.salary, concat(m.first_name, m.last_name) AS manager
--> FROM employee e
--> JOIN role ON e.role_id=role.id
--> JOIN department on role.department_id = department.id
--> JOIN employee m ON m.id = e.manager_id;`, function (err, results) {
-    console.log(results);
+// //View All Employees
+
+const viewEmployee = () => {
+  const sql = `SELECT
+  -> e.id, e.first_name, e.last_name, role.title, department.name, role.salary, concat(m.first_name, m.last_name) AS manager
+  -> FROM employee e
+  -> JOIN role ON e.role_id=role.id
+  -> JOIN department on role.department_id = department.id
+  -> JOIN employee m ON m.id = e.manager_id`;
+  db.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    console.table(results);
+    start();
   });
+};
+
+// //Add a department
+
+const addDept = () => {
+  inquirer.prompt({
+    name: "department",
+    type: "input",
+    message: "What is the name of department you want to add?"
+  })
+    .then((answers) => {
+      const sql = `INSERT INTO department(name) VALUES(?)`;
+      db.query(sql, answers.department, (err, results) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+          return;
+        }
+        console.log(`${answer.department} successfully added!`);
+      })
+    }
+    )
+};
 
 
+// //Add an employee
 
+
+// //Remove an Employee
+
+// //Update an Employee Role
